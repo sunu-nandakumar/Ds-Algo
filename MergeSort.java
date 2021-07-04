85e7cd451b534974d17e1e544f2d240f8e709981
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-07-04 13:07:38 +0530

    adding the merge sort (problem 5)

---
+++ MergeSort.java
@@ -0,0 +1,90 @@
+package ds_algo;
+
+import java.util.Scanner;
+
+public class MergeSort {
+
+	private static Scanner sc;
+	void merge(int arr[], int l, int m, int r) {
+		// L is the start index and m is the middle part where we cut the array and r is the last of array
+		// Find sizes of two subarrays to be merged
+		int n1 = m - l + 1;
+		int n2 = r - m;
+
+		/* Create temp arrays */
+		int L[] = new int[n1];
+		int R[] = new int[n2];
+
+		/* Copy data to temp arrays */
+		for (int i = 0; i < n1; i++)
+			L[i] = arr[l + i];
+		for (int j = 0; j < n2; j++)
+			R[j] = arr[m + 1 + j];
+
+		/* Merge the temp arrays */
+
+		// Initial indexes of first and second subarrays
+		int i = 0, j = 0;
+
+		// Initial index of merged subarray array
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		/* Copy remaining elements of L[] if any */
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		/* Copy remaining elements of R[] if any */
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+	void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m =l+ (r-l)/2;
+ 
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int[] arr = new int[10];
+		System.out.println("Enter the elements of the array: ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+        printArray(arr);
+
+	}
+
+}
